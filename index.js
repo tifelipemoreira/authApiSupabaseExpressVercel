5df1084f248550b5c00a1548f9deb6d05811eb4a
Felipe Moreira <ti.felipemoreira@gmail.com>
2022-04-25 14:28:23 -0300

    melhoria

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = require('./src/config/express')
 const port = process.env.PORT || 4000 
 
 const users = require('./src/api/users')
-app.get('/userbyid', users.getUsersbyId)
+
 app.get('/userbyemail', users.getUsersbyEmail)
-app.get('/getUserBd', users.getUserBd)
+app.get('/getuserbyid', users.getUserById)
 app.listen(port)
