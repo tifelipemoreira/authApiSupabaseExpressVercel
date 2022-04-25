c79d3b249bf208bb92230f92cd1b89675f8e2d84
Felipe Moreira <ti.felipemoreira@gmail.com>
2022-04-25 13:07:29 -0300

    ajuste erro

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./src/config/express')
 
 const port = process.env.PORT || 4000 
 
-const user = require('./src/api/users')
+const users = require('./src/api/users')
 app.get('/users', users.users)
 app.get('/users2', users.users2)
 
