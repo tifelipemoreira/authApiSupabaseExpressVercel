01d5c88ef95788ce116e6b2afcecfd07e28d035d
Felipe Moreira <ti.felipemoreira@gmail.com>
2022-05-18 21:47:52 -0300

    melhorias bcript entre outras

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger.json')
 const app = require('./src/config/express')
+require('dotenv/config');
 
-const port = process.env.PORT || 4000 
+const port = process.env.PORT || 3000 
 
 const users = require('./src/api/users')
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.get('/userbyemail', users.getUsersbyEmail)
 app.get('/getuserbyid', users.getUserById)
 app.post('/createuser', users.postCreateUser)
+app.post('/oauth2',users.oauth2)
 app.listen(port)
--- src/api/users.js
+++ src/api/users.js
@@ -1,4 +1,5 @@
 const pool = require("../config/dbPostgress");
+const bcrypt = require("bcrypt");
 
 const getUsersbyEmail = async (req, res) => {
   console.log("Ping function");
@@ -9,7 +10,6 @@ const getUsersbyEmail = async (req, res) => {
 
 const postCreateUser = async (req, res) => {
   const { email, password, user_name, fullname, erp_user_code } = req.body;
-  const id = 1;
 
   //validar se todos os campos do body foram informados.
   if (!email) {
@@ -44,7 +44,7 @@ const postCreateUser = async (req, res) => {
     return res.status(500).json(alreadyExists.e);
   }
   if (!alreadyExists.continue) {
-    return res.status(400).json({
+    return res.status(409).json({
       status: "email,erp_user_code or user_name already exists",
     });
   }
@@ -63,7 +63,7 @@ const postCreateUser = async (req, res) => {
     console.log(userCreated);
     return res.status(200).json({
       status: "user created",
-      data: { user_name: userCreated.user_name },
+      data: { id: userCreated.id },
     });
   }
 };
@@ -77,11 +77,11 @@ const getUserById = async (req, res) => {
   var id = req.query.id;
   try {
     pool.query(
-      `SELECT id,erp_user_code,email FROM USERS WHERE id = '${id}' `,
+      `SELECT id,erp_user_code,email FROM users WHERE id = '${id}' `,
       (error, result) => {
         //retorna objeto com resultado da query
         if (typeof error != "undefined") {
-          //console.log("erro")
+          console.log(error);
           return res.status(400).json({ error });
         }
         //console.log("funcionou")
@@ -100,71 +100,74 @@ const getUserById = async (req, res) => {
 async function checkUserAlreadyExists(email, erp_user_code, user_name) {
   var response;
   var res;
+  console.log(
+    `select count(*) from users where  email = '${email.trim()}' or erp_user_code =  '${erp_user_code.trim()}' or user_name = '${user_name.trim()}'`
+  );
   try {
     response = await pool.query(
-      `select count(*) from users where  email = '${email.trim()}' or erp_user_code =  '${erp_user_code.trim()}' or user_name = '${user_name.trim()}'`
+      `select count(*) quantidade from users where  email = '${email.trim()}' or erp_user_code =  '${erp_user_code.trim()}' or user_name = '${user_name.trim()}'`
     );
+    console.log("count", response.rows[0].quantidade);
   } catch (e) {
-    console.log("capturou o erro");
+    console.log("1");
     res = { continue: false, error: true, e };
     return res;
   }
-  if (response.rows["count"] > 0) {
-    console.log("retorno deu erro e ja existe", response.rows["count"]);
+  if (response.rows[0].quantidade > 0) {
+    console.log("2");
     return { continue: false, error: false };
   }
-  console.log("retorno deu  existe");
+  console.log("3");
   return { continue: true, error: false };
 }
 
 async function createUser(email, erp_user_code, password, user_name, fullname) {
   var response;
-  var insert = `insert into users`;
+  var insert;
+  var bcryptPassword = "";
+  // utiliza biblioteca bcrypt para criptografar a senha
+  bcrypt.hash(password, 10, (errBcrypt, hash) => {
+    if (errBcrypt) {
+      return (res = { continue: false, error: true, errBcrypt });
+    }
+    bcryptPassword = hash;
+    console.log("bcrypt", bcryptPassword);
+  });
+  
+  insert = `insert into users`;
   insert = insert + `(email,erp_user_code,password,user_name,fullname)`;
   insert =
     insert +
-    `values  ('${email.trim()}','${erp_user_code.trim()}','${password.trim()}','${user_name.trim()}','${fullname.trim()}') RETURNING id`;
+    `values  ('${email.trim()}','${erp_user_code.trim()}','${bcryptPassword}','${user_name.trim()}','${fullname.trim()}') RETURNING id`;
   console.log(insert);
   try {
-    console.log("entrou insert");
+    console.log("4");
     response = await pool.query(insert);
   } catch (e) {
-    console.log("capturou o erro");
+    console.log("5");
     res = { continue: false, error: true, e };
     return res;
   }
-  console.log("retorno deu  existe");
-  return response;
+  console.log(response.rows[0].id);
+  return { id: response.rows[0].id, error: false };
 }
+
+//função de autenticação retorna token
+const oauth2 = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    response = await pool.query(
+      `select count(*) quantidade from users where  email = '${email.trim()}' and password = '${password.trim()}'`
+    );
+    return res.status(200).json({ email: email });
+  } catch (e) {
+    return res.status(400).json({ error: "credentials wrong" });
+  }
+};
 
 module.exports = {
   getUserById,
   getUsersbyEmail,
   postCreateUser,
+  oauth2,
 };
-/*
- 
-try {
-    console.log("nao deveria chegar no insert");
-    pool.query(
-      //`SELECT id,erp_user_code,email FROM USERS WHERE id = '${id}' `,
-      `insert into users (email,erp_user_code,password,user_name,fullname) values  ('${email}','${erp_user_code}','${password}','${user_name}','${fullname}');`,
-      (error, result) => {
-        console.log("dentro do insert");
-        //retorna objeto com resultado da query
-        console.log(error);
-        if (typeof error != "undefined") {
-          //console.log("erro")
-          return res.status(400).json({ error });
-        }
-        //console.log("funcionou")
-        return res.status(200).json({
-          status: "User Created",
-        });
-      }
-    );
-  } catch (e) {
-    console.log(e);
-    return res.status(400).json({ e });
-  }
-  */
